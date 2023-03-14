6b6316342964ae6d46c2245ee1d95746ef931f2f
aditya <aa1633350@gmail.com>
2023-03-14 21:29:31 +0530

    Added skeleton functions to Game class.

--- src/Game.java
+++ src/Game.java
@@ -5,4 +5,26 @@ public class Game {
     private Player currentPlayer;
     private Board board;
     private Player winner;
+
+    public void startGame() {
+        // logic to start the game
+    }
+
+    public void endGame() {
+        // logic to end the game
+    }
+
+    public void switchTurn() {
+        // logic to switch the current player
+    }
+
+    public void makeMove(Move move) {
+        // logic to make a move on the game board
+    }
+
+    public Player checkForWinner() {
+        // logic to check for a winner
+        // returns the winning player or null if there is no winner
+        return null;
+    }
 }
